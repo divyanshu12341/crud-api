1e691d405cca7ffb3232cc7f0994ef782b6f9bd2
Divyanshu Jain <divyanshujain111@gmail.com>
2023-09-11 11:07:12 +0530

    Made changes in routes

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {v4:uuid} = require("uuid");
 const {updateUser,createUser,getUserById,deleteUser} = require('../controllers/users');
-let usersModel = [
-  {
-    firstName: "Divyanshu",
-    lastName: "Jain",
-    age: 23,
-  },
-];
 router.get("/", (req, res) => {
   console.log(usersModel);
   res.send(usersModel);
