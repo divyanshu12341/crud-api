d30eabd8b1e67a5b7b8fce0eea8e7100cc2c95f5
Divyanshu Jain <divyanshujain111@gmail.com>
2023-09-11 06:37:28 +0530

    Created a post route which adds user to database

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const router =express.Router();
-const usersModel = [{
-    firstName:"Divyanshu",
-    lastName:"Jain",
-    age:23,
-}];
-router.get('/',(req,res)=>{
-    console.log(usersModel);
-    res.send(usersModel);
-})
+const router = express.Router();
+const usersModel = [
+  {
+    firstName: "Divyanshu",
+    lastName: "Jain",
+    age: 23,
+  },
+];
+router.get("/", (req, res) => {
+  console.log(usersModel);
+  res.send(usersModel);
+});
+router.post('/',(req,res)=>{
+    const user = req.body;
+    usersModel.push(user);
+    res.send(`User with name ${user.firstName} added to the database`);
+}) ;
 module.exports = router;
